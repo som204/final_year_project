21cb6348d0e11adbe4afd9e2f72440e309fbbfd6
Rupsa <iamrupsasarkargmail.com>
2025-09-15 23:37:07 +0530

    rupsa

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,9 +1,9 @@
 import React from "react";
-import Login from "./components/Login";
-import Signup from "./components/Signup";
+import Login from "./pages/Login";
 import Home from "./components/Home";
 import DataUpload from "./components/DataUpload";
 import Dashboard from "./components/Dashboard";
+import Register from "./pages/Register";
 
 function App() {
   return (
@@ -12,10 +12,11 @@ function App() {
       {/* <Home /> */}
 
       {/* Later, you will show Login first, then after success -> Dashboard */}
-       {/* <Login />  */}
+       <Login /> 
        {/* <Signup />  */}
       {/* <DataUpload />  */}
-      <Dashboard />
+      {/* <Dashboard /> */}
+      {/* <Register /> */}
     </>
   );
 }
