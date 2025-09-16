3a694f797ff860b73bf9e8deba3bda687e0f666a
som <somprasad240@gmail.com>
2025-09-17 00:27:27 +0530

    Fixed frontend api fetch issue

--- Backend/main.py
+++ Backend/main.py
@@ -3,10 +3,17 @@ from Routes.user_routes import router as user_router
 from Routes.institute_routes import router as institute_router
 from Routes.department_routes import router as department_router
 from Middleware.auth_middleware import AuthMiddleware
+from fastapi.middleware.cors import CORSMiddleware
 
 app=FastAPI()
 
-
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  
+    allow_credentials=True,
+    allow_methods=["*"],  
+    allow_headers=["*"],
+)
 
 
 app.add_middleware(AuthMiddleware)
