ceb5ea9c65efc344be793890824ae3ef10285a67
som <somprasad240@gmail.com>
2025-09-20 23:43:44 +0530

    Fixed git merge conflict 2

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,7 +1,16 @@
 // src/App.jsx
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-
+import React from "react";
+import Login from "./pages/Login";
+import HomePage from "./pages/HomePage";
+import DataUpload from "./pages/DataUpload";
+import Dashboard from "./components/Dashboard";
+import Register from "./pages/Register";
+import FacultyDashboard from "./pages/FacultyDashboard";
+import StudentDashboard from "./pages/StudentDashboard";
+import AdminDashboard from "./pages/AdminDashboard";
+import SuperAdmin from "./pages/SuperAdmin";
 import Login from "./pages/Login";
 import DataUpload from "./pages/DataUpload";
 import Register from "./pages/Register";
@@ -22,8 +31,13 @@ function App() {
         <Route path="/faculty-dashboard" element={<FacultyDashboard />} />
         <Route path="/register-staff" element={<RegisterStaff />} />
         <Route path="/register-student" element={<RegisterStudent />} />
+        <Route path="/student-dashboard" element={<StudentDashboard />} />
+        <Route path="/admin-dashboard" element={<AdminDashboard />} />
+        <Route path="/super-admin" element={<SuperAdmin />} />
+        <Route path="/home" element={<HomePage />} />
       </Routes>
     </Router>
+    
   );
 }
 
